4d9819318f16c8e980069687b4b30ab4fb505ebf
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-09-10 00:33:26 +0300

    getdatafromjason

--- index.js
+++ index.js
@@ -13,6 +13,46 @@ button.addEventListener('click',()=>{
     
     else{
         text.innerText='Welcome '+input.value+' !';
+    
+        fetch('https://api.genderize.io/?name='+input.value).then((response) => {
+        console.log('gender',response);
+        return response.json();
+        
+        }).then(data =>{
+        console.log(data);
+        document.write(data.gender);
+        }).catch((error)=> {
+        console.log('rejected',err);
+        })
+        
+        fetch('https://api.agify.io/?name='+input.value).then((response) => {
+        console.log('age',response);
+        return response.json();
+        }).then(data =>{
+        console.log(data);
+        document.write(data.age);
+        }).catch((error)=> {
+        console.log('rejected',err);
+        })
+
+        fetch('https://api.nationalize.io/?name='+input.value).then((response) => {
+        console.log('nationality',response);
+        return response.json();
+        }).then(data =>{
+        console.log(data);
+        }).catch((error)=> {
+        console.log('rejected',err);
+        })
+        
+        fetch('https://dog.ceo/api/breeds/image/random').then((response) => {
+        console.log('dog',response);
+        return response.json();
+        }).then(data =>{
+        console.log(data);
+        }).catch((error)=> {
+        console.log('rejected',err);
+        })
+    
     }
         
 })
