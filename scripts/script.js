699f2ef3f6c91e7b129402c20afc2f31c4110c71
Karam Al Ashkar <karam.alachkar16@gmail.com>
2022-09-10 10:27:48 +0300

    final-clean-code

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//connect to all HTML tags needed
 const button=document.querySelector('#but');
 const input=document.querySelector('#input');
 const text=document.querySelector('#text');
@@ -6,8 +7,9 @@ const age=document.querySelector('#age');
 const country=document.querySelector('#country');
 const img=document.querySelector('#image');
 
+//waiting click event on the button
 button.addEventListener('click',()=>{
-    if(input.value == ''){
+    if(input.value == ''){ //check if the user has entered a name
         button.style.backgroundColor='red';
         button.innerHTML='RESTART';
         input.disabled=true;
@@ -16,45 +18,49 @@ button.addEventListener('click',()=>{
     
     else{
         text.innerText='Welcome '+input.value+' !';
-    
+        
+        //fetch in API to get a predicted gender for the user
         fetch('https://api.genderize.io/?name='+input.value).then((response) => {
         console.log('gender',response);
-        return response.json();
+        return response.json(); //return the json form
         
         }).then(data =>{
         console.log(data);
-        gender.innerText='Your predict gender: ' + data.gender;
+        gender.innerText='Your predict gender: ' + data.gender; //send data to HTML tag
         }).catch((error)=> {
         console.log('rejected',err);
         })
         
+        //fetch in API to get a predicted age for the user
         fetch('https://api.agify.io/?name='+input.value).then((response) => {
-        console.log('age',response);
-        return response.json();
+        console.log('age',response); 
+        return response.json(); //return the json form
         }).then(data =>{
         console.log(data);
-        age.innerText='Your predict age: ' + data.age;
+        age.innerText='Your predict age: ' + data.age; //send data to HTML tag
         }).catch((error)=> {
         console.log('rejected',err);
         })
 
+        //fetch in API to get a predicted country for the user
         fetch('https://api.nationalize.io/?name='+input.value).then((response) => {
         console.log('nationality',response);
-        return response.json();
+        return response.json(); //return the json form
         }).then(data =>{
         console.log(data);
-        country.innerText='Your predict country: ' + data.country[0].country_id + ' - ' + data.country[1].country_id;
+        country.innerText='Your predict country: ' + data.country[0].country_id + ' - ' + data.country[1].country_id; //send data to HTML tag
         }).catch((error)=> {
         console.log('rejected',err);
         })
-        
+
+        //fetch in API for a dog picture        
         fetch('https://dog.ceo/api/breeds/image/random').then((response) => {
         console.log('dog',response);
-        return response.json();
+        return response.json(); //return the json form
         }).then(data =>{
         console.log(data);
-        img.style.display='block';
-        img.src=data.message;
+        img.style.display='block'; //send data to HTML tag
+        img.src=data.message; //send data to HTML tag
         }).catch((error)=> {
         console.log('rejected',err);
         })
